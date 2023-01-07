9645de8df291863e66d253792cf6bb8c79af54a4
Guido Curcio <guidomaurocurcio@gmail.com>
2023-01-07 17:30:09 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Header></Header>
       {/* <body className="relative h-full w-full bg-[#151415fc] bg-opacity-[100%]"> */}
       <body className="relative h-full w-full bg-[#020d16] bg-opacity-[100%]">
-        <div className="bg-newy h-full absolute z-0 w-full brightness-[60%]"></div>
+        <div className="bg-newy h-screen absolute z-0 w-full brightness-[30%]"></div>
         <div className="relative z-0 h-full overflow-x-hidden">{children}</div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,48 @@
-import { Footer } from "components/Footer";
-import { Hero } from "components/Hero";
 import { Soundy } from "components/SoundGrad";
-import { VideoWebsite } from "components/VideoWebsite";
 import { twMerge } from "tailwind-merge";
 
 export default function Page() {
   return (
-    <div className="mx-auto overflow-hidden">
-      <div className="mx-auto">
-        <div className=" h-[1500px] max-w-[1200px] mx-auto w-[1200px] overflow-hidden rounded-[5px] border-r border-l border-sharper border-opacity-5  bg-fondy bg-opacity-[80%] relative ">
-          <Soundy
-            pathOpacity="0.5"
-            strokeWidth={0.5}
-            viewbox="0 0 800 800"
-            className={twMerge(
-              "h-[1500px] w-[2100px] -translate-y-[100px] -translate-x-[300px] rotate-[130deg] scale-y-[1]",
-              "scale-x-[-1] fill-none opacity-[80%] blur-[0px] brightness-[188%] contrast-[110%] hue-rotate-[80deg] saturate-[230%] sepia-[40%]"
-            )}
-          ></Soundy>{" "}
-          <Hero></Hero>
-          {/* <Footer></Footer> */}
+    <div className="pt-[50px] max-w-[1300px] mx-auto">
+      <div className="relative h-[800px] overflow-hidden rounded-[5px] border border-sharper border-opacity-10  bg-black bg-opacity-[95%] backdrop-blur-[10px] font-visuelt shadow-3xl shadow-[#6d432452] ">
+        <Soundy
+          pathOpacity="0.4"
+          strokeWidth={0.5}
+          viewbox="0 0 800 800"
+          className={twMerge(
+            "h-[1200px] absolute w-[1300px] -translate-y-[100px] z-5 -translate-x-[200px] rotate-[-80deg] scale-y-[-1] ",
+            "scale-x-[-1] fill-none opacity-[100%] blur-[0px] brightness-[98%] contrast-[110%] hue-rotate-[-310deg] saturate-[110%] sepia-[20%]"
+          )}
+        ></Soundy>{" "}
+        <Soundy
+          pathOpacity="0.4"
+          strokeWidth={0.5}
+          viewbox="0 0 800 800"
+          className={twMerge(
+            "h-[1200px] absolute w-[1300px] translate-y-[108px] z-5 translate-x-[981px] rotate-[-80deg] scale-y-[1] ",
+            "scale-x-[-1] fill-none opacity-[100%] blur-[0px] brightness-[58%] contrast-[110%] hue-rotate-[-310deg] saturate-[110%] sepia-[20%]"
+          )}
+        ></Soundy>{" "}
+        <div className="relative h-[700px] font-visuelt ">
+          <h5 className="pt-[20px] text-center stroky cursor-pointer font-SpaceGrotesk text-[24px] lowercase opacity-100">
+            <span className="font-normal  tracking-[-3px] text-sharper text-opacity-[95%] shadow-2xl">
+              obtuso.com
+            </span>
+          </h5>
+          <div className="tracking-[-4px] mt-[35px] leading-[80px] font-visuelt z-50 text-white   stroky hero-grad2">
+            <p className="text-[80px] font-[400] text-center text-shadow z-50 hero-grad text-opacity-[105%] ">
+              <span className="">Serverless API infrastructure</span>
+              <br></br>
+              <span className="">for the web</span>
+            </p>
+          </div>
+          <div className="tracking-[-1px] text-[18px] text-sharper text-opacity-[100%] leading-[20px] w-[420px] text-center mx-auto pt-[20px] font-calibre">
+            <p className="text-center ">
+              A platform that gives you deployment to the edge, code-generation,
+              one-off pages and websites, tracking, events, crons, and more.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="absolute top-[450px] left-[250px] z-50 space-x-[50px] grid grid-flow-col">
-        <VideoWebsite
-          websiteDomain="imdb.com"
-          videoSRC="https://tailwindcss.com/_next/static/media/column.com.b51c82b855e43d930ae0ef0bd15579df.mp4"
-        ></VideoWebsite>
-        <VideoWebsite
-          websiteDomain="netflix.com"
-          videoSRC="https://tailwindcss.com/_next/static/media/theverge.com.3275ed9cce231ccfe09a580b22cb46b5.mp4"
-        ></VideoWebsite>
-        <VideoWebsite
-          websiteDomain="airbnb.com"
-          videoSRC="https://tailwindcss.com/_next/static/media/githubnext.com.ba9e28d9bf1694795b129aca83ede8ab.mp4"
-        ></VideoWebsite>
-        <VideoWebsite
-          websiteDomain="instagram.com"
-          videoSRC="https://tailwindcss.com/_next/static/media/wander.com.8689ef42942b161401070163c1549660.mp4"
-        ></VideoWebsite>
-        {/* <VideoWebsite
-          websiteDomain="instagram.com"
-          videoSRC="https://tailwindcss.com/_next/static/media/wander.com.8689ef42942b161401070163c1549660.mp4"
-        ></VideoWebsite>
-        <VideoWebsite
-          websiteDomain="instagram.com"
-          videoSRC="https://tailwindcss.com/_next/static/media/wander.com.8689ef42942b161401070163c1549660.mp4"
-        ></VideoWebsite> */}
       </div>
     </div>
   );
