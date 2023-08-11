84cd2ff82169e28a461b62572f3500fb194f9461
guicurcio <guidomaurocurcio@gmail.com>
2023-08-11 19:27:58 -0300

    ...

---
+++ src/app/podsfy copy/page.tsx
@@ -0,0 +1,9 @@
+import BlogComponent from "components/BlogComponent";
+
+export default function Page() {
+  return (
+    <div className="grid grid-flow-col gap-0 items-start h-screen w-screen place-items-start">
+      <BlogComponent></BlogComponent>
+    </div>
+  );
+}
