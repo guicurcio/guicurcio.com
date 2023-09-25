0189cae04014d6dbde4aa083f9dd324e70ebfb10
guicurcio <guidomaurocurcio@gmail.com>
2023-09-25 16:39:13 -0300

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,38 +109,54 @@ export default function Page() {
         </div>
         <div className="grid grid-flow-col grid-cols-3 row-span-1  gap-[3px] w-full h-full max-h-full max-w-full">
           <div className={mergeClasses(...BORDER_POSTS_CLASSNAMES, "")}>
-            <div className="grid grid-flow-row place-content-around grid-rows-6">
+            <div className="grid grid-flow-row   grid-rows-6">
               <div className="grid grid-flow-row  row-span-1  place-content-end content-baseline">
                 <h1 className="font-visuelt justify-end w-fit pt-[20px] pr-[18px] text-[#F2F2F2] text-[18px] font-normal leading-[24px]">
                   Engineering
                 </h1>
               </div>
 
-              <div className="grid pl-[30px]  items-start relative row-span-4">
-                <div className="grid grid-flow-row rounded-[12px] border border-opacity-20 border-white   items-start px-[20px]">
-                  <div className="grid grid-flow-col gap-[5px] pl-[0px] pt-[15px] items-start content-start place-content-start">
+              <div className="grid   pl-[30px] pr-[80px] w-full  items-start relative row-span-4">
+                <div className="absolute top-[-25px] right-[50px]">
+                  <Image
+                    className=" w-[74px] h-[72px]"
+                    src="/assets/socials/github-logo-big.svg"
+                    alt="asd"
+                    width={74}
+                    height={72}
+                    quality={100}
+                  ></Image>
+                </div>
+                <div className="grid w-full grid-flow-row rounded-[12px] border border-opacity-20 border-white   items-start px-[20px]">
+                  <div className="grid grid-flow-col gap-[5px] pl-[0px] pt-[15px] items-start content-start place-content-start w-full">
                     <div className="rounded-full w-4 h-4 border border-white border-opacity-20"></div>
                     <div className="rounded-full w-4 h-4 border border-white border-opacity-20"></div>
                     <div className="rounded-full w-4 h-4 border border-white border-opacity-20"></div>
                   </div>
-                  <div className="grid grid-flow-row place-content-start pt-[20px] pb-[30px]">
+                  <div className="grid w-full grid-flow-row place-content-start pt-[20px] pb-[30px] ">
                     <p className="font-mono text-[#ACACAF] text-[14px] font-medium  leading-[22px] tracking-[-0.175px]">
                       <span className="text-[#FF7B72]">$</span>{" "}
                       <span className="text-[#FF7B72]">git show</span> branch
                       delete beam fix-timestamps
                     </p>
-                    <p className="font-mono text-[#ACACAF] text-[14px] font-medium  leading-[22px] tracking-[-0.175px]">
+                    <p className="font-mono text-[#ACACAF] text-[14px] font-medium  leading-[22px] tracking-[-0.175px] pt-[30px]">
                       <span className="text-[#FF7B72]">$</span>{" "}
-                      <span className="text-[#FF7B72]">git show</span> branch
-                      delete beam fix-timestamps
+                      <span className="text-[#FF7B72]">git show</span>
                     </p>
                     <p className="font-mono text-[#ACACAF] text-[14px] font-medium  leading-[22px] tracking-[-0.175px]">
-                      NAME              PARENT     PRODUCTION   CREATED AT
-                      ----------------- ---------- ------------ ---------------
-                      main              n/a        Yes          4 months ago
-                      add-upvotes       main       No           1 minute ago
-                      user-index        main       No           2 days ago
-                      revert-comments   main       No           8 days ago
+                      NAME PARENT PRODUCTION CREATED AT
+                    </p>
+                    <p className="font-mono text-[#ACACAF] text-[14px] font-medium  leading-[22px] tracking-[-0.175px]">
+                      ----- ----- --------- ----------
+                    </p>
+                    <p className="font-mono text-[#ACACAF] text-[14px] font-medium  leading-[22px] tracking-[-0.175px]">
+                      boilerplate typescript 2021-08-01 12:00:00
+                    </p>
+                    <p className="font-mono text-[#ACACAF] text-[14px] font-medium  leading-[22px] tracking-[-0.175px]">
+                      boilerplate typescript 2021-08-01 12:00:00
+                    </p>
+                    <p className="font-mono text-[#ACACAF] text-[14px] font-medium  leading-[22px] tracking-[-0.175px]">
+                      boilerplate typescript 2021-08-01 12:00:00
                     </p>
                   </div>
                 </div>
@@ -154,9 +170,21 @@ export default function Page() {
                     30 pieces
                   </h1>
                 </div>
-                <div className="grid grid-flow-row items-start">
-                  <div className="absolute bottom-0 right-0 z-50 w-[300px] h-[230px] bg-[#111213] border-t border-l border-[#30363D] rounded-tl-[8px]">
-                    <div className="bg-[#0A0808] h-full w-full pl-[8px]">
+                <div className="grid grid-flow-row items-start overflow-hidden">
+                  <div className="absolute bottom-0 right-0 z-50  bg-[#111213] border-t border-l border-[#30363D] rounded-tl-[8px]">
+                    <div className="w-full pl-[6px] pt-[5px] grid grid-flow-col items-start content-start place-items-start place-content-start">
+                      <div className="bg-[#08090A] rounded-t-[6px] w-fit h-fit border-t-0.5 border-white/10 px-[10px]">
+                        <h1 className="font-mono text-[13px] font-medium text-white">
+                          layout.tsx
+                        </h1>
+                      </div>
+                      <div className="bg-[#08090A10] rounded-t-[6px] w-fit h-fit border-t-0.5 border-white/10 px-[10px]">
+                        <h1 className="font-mono text-[13px] font-medium text-white">
+                          app.tsx
+                        </h1>
+                      </div>
+                    </div>
+                    <div className="bg-[#0A0808] h-full w-full pl-[8px] pt-[4px]">
                       <p className="font-mono text-[#ACACAF] text-[14px] font-medium  leading-[22px] tracking-[-0.175px]">
                         1 <span className="text-[#FF7B72]">import</span>{" "}
                         matplotlib.pyplot{" "}
