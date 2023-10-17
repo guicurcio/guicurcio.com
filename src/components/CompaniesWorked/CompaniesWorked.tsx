c8781976108a3cf690909e20ae5bf5ca9fa29bf8
guicurcio <guidomaurocurcio@gmail.com>
2023-10-17 13:24:43 -0300

    asd

--- src/components/CompaniesWorked/CompaniesWorked.tsx
+++ src/components/CompaniesWorked/CompaniesWorked.tsx
@@ -23,11 +23,8 @@ export default function CompaniesWorked({
     <div className={mergeClasses(...BORDER_POSTS_CLASSNAMES, "col-span-3")}>
       <div className="grid grid-flow-row place-content-around grid-rows-6 grid-cols-1 w-full">
         <div className="grid grid-flow-row  pt-[18px] px-[18px]   row-span-1  col-span-1 ">
-          <h1 className="font-visuelt   text-center  text-[#FFFFFF] text-[18px] font-normal leading-[24px]">
-            Companies
-          </h1>
-          <h1 className="font-visuelt   text-center  text-[#FFFFFF] text-[18px] font-normal leading-[24px]">
-            I've worked with
+          <h1 className="font-visuelt  w-fit align-middle  text-[#F2F2F270] text-[18px] font-normal leading-[24px]">
+            Companies I've worked with
           </h1>
         </div>
         <div className=" grid grid-flow-row  row-span-5 w-full  overflow-hidden "></div>
